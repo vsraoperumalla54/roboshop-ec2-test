74d67f90a6a1039a09d9138247ff64def59febc3
venkata.perumalla <venkata.perumalla@pragmaticplay.com>
2025-11-05 01:59:12 +0530

    terraform-ec2-test

---
+++ ec2.tf
@@ -0,0 +1,27 @@
+module "catalogue" {
+    source = "../terraform-aws-instance"
+    # ami_id = "ami-09c813fb71547fc4f"
+    # sg_ids = ["sg-07c8acf3fa6b923fa"]
+    # instance_type = "t3.micro"
+    # tags = {
+    #     Name = "modu"
+    # }
+
+    ami_id = var.ami_id
+    sg_ids = var.sg_ids
+    instance_type = var.instance_type
+    tags = var.tags
+    
+}
+
+output "pub_ip" {
+    value = module.catalogue.public_ip
+}
+
+output "priv_ip" {
+    value = module.catalogue.private_ip
+}
+
+output "id" {
+    value = module.catalogue.instance_id
+}
