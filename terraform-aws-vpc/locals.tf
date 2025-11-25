a4dc0f97e69b8309b50d2ce7fd6fcd33c024b9ba
venkata.perumalla <venkata.perumalla@pragmaticplay.com>
2025-11-25 23:25:39 +0530

    terraform

--- terraform-aws-vpc/locals.tf
+++
@@ -1,9 +0,0 @@
-locals{
-    common_tags = {
-        Project = var.project_name
-        Environment = var.environment
-        Terraform = true
-    }
-    common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
-    # az_names = slice(data.aws_availability_zones.available.names, 0, 2 )
-}
